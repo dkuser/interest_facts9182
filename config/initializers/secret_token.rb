93cd7497f0605422ac2f408aa5b42a8649b68e63
unknown <D:\Program Files\The Bat!\Mail>
2011-07-26 23:05:28 +0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InterestFacts::Application.config.secret_token = 'af1617d73bf3014a3672764899b7f878234daa451b22c14a9b9277bece20d304fe221cfadb50793b08e886102c6c8cb36496f494c3aed30c00868920c82c6547'
