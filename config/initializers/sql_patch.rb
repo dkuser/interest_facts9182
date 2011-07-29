ded59c4f8a8ceb17c6686a172d052b4c0888cc18
dkuser <D:\Program Files\The Bat!\Mail>
2011-07-30 00:06:18 +0400

    'new'

---
+++ config/initializers/sql_patch.rb
@@ -0,0 +1,27 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'mysql'
+
+class Mysql::Result
+  def encode(value, encoding = "utf-8")
+    String === value ? value.force_encoding(encoding) : value
+  end
+
+  def each_utf8(&block)
+    each_orig do |row|
+      yield row.map {|col| encode(col) }
+    end
+  end
+  alias each_orig each
+  alias each each_utf8
+
+  def each_hash_utf8(&block)
+    each_hash_orig do |row|
+      row.each {|k, v| row[k] = encode(v) }
+      yield(row)
+    end
+  end
+  alias each_hash_orig each_hash
+  alias each_hash each_hash_utf8
+end
