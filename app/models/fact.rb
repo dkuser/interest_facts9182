f2cb3c85fbcb27a4ef1f2e0b4aa427c4b1c69468
dkuser <D:\Program Files\The Bat!\Mail>
2011-07-30 01:33:01 +0400

    'sf'

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,5 +1,5 @@
 class Fact < ActiveRecord::Base
-  scope :random_record,     order("RAND()").limit(1)
+  scope :random_record,     order("RANDOM()").limit(1)
   
   def self.random_record_s 
     random_record.first
