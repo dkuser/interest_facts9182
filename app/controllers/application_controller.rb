4e257fe873c888552dabcc65e751754de380aab4
dkuser <D:\Program Files\The Bat!\Mail>
2011-07-30 15:33:26 +0400

    'sdf'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :ensure_domain
+
+  @@APP_DOMAIN = 'interfact.net'
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] != @@APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{@@APP_DOMAIN}", :status => 301
+    end
+  end
+  
+  def ApplicationController.domain
+    @@APP_DOMAIN
+  end
 end
